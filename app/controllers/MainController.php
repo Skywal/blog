a7840b151c0561ca995af398e347411483484709
Andrew <Skaywoker6@gmaill.com>
2019-07-27 16:34:10 +0300

    Make article output.

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -12,9 +12,21 @@ class MainController extends AppController
 {
 
 	public function indexAction(){
+        $articles = \R::findAll('articles', 'ORDER BY `pubdate`');
 
 		$this->setMeta(App::$app->getProperty('site_name'),
 			'Page description',
 			'Page keywords');
+
+		$this->beautifyText($articles);
+
+		$this->set(compact('articles'));
+
 	}
+
+	protected function beautifyText($arr, $txtLength = 255){
+	    foreach ($arr as $elem){
+	        $elem->text = substr($elem->text, 0, $txtLength) . ' ...';
+        }
+    }
 }
