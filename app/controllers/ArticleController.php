1cbdf0b181b7fae68349b06110549c3b30d7df4e
Bloom <skaywoker6@gmail.com>
2019-08-02 09:59:30 +0300

    Make small fix.

--- app/controllers/ArticleController.php
+++ app/controllers/ArticleController.php
@@ -4,12 +4,23 @@
 namespace app\controllers;
 
 
+use mysql_xdevapi\Exception;
+
 class ArticleController extends AppController
 {
     public function viewAction(){
         $alias = $this->route['alias'];
         $article = \R::findOne('articles', 'alias = ?', [$alias]);
 
-        debug($article);
+        if(!$article){
+            throw new Exception('Page not found', 404);
+        }
+
+        // галерея
+
+        // коментарі
+
+        $this->setMeta($article->title);
+        $this->set(compact('article'));
     }
 }
--- app/views/Article/view.php
+++ app/views/Article/view.php
@@ -1,1 +1,24 @@
-Article
+<div class="single-grid">
+    <img src="images/<?=$article->image;?>" alt=""/>
+    <p><h2><?=$article->title;?></h2> </p>
+    <p><?=nl2br($article->text);?></p>
+</div>
+<!--<ul class="comment-list">-->
+<!--    <h5 class="post-author_head">Written by <a href="#" title="Posts by admin" rel="author">admin</a></h5>-->
+<!--    <li><img src="images/avatar.png" class="img-responsive" alt="">-->
+<!--        <div class="desc">-->
+<!--            <p>View all posts by: <a href="#" title="Posts by admin" rel="author">admin</a></p>-->
+<!--        </div>-->
+<!--        <div class="clearfix"></div>-->
+<!--    </li>-->
+<!--</ul>-->
+<div class="content-form">
+    <h3>Leave a comment</h3>
+    <form>
+        <input type="text" placeholder="Name" required/>
+        <input type="text" placeholder="Email" required/>
+        <input type="text" placeholder="Phone" required/>
+        <textarea placeholder="Message"></textarea>
+        <input type="submit" value="SEND"/>
+    </form>
+</div>
